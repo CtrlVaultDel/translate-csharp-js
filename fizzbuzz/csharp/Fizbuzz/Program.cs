6b0cf624bf4a88c60ea13d6caa9592ca5f2bf673
CtrlVaultDel <ctrlvaultdel51@gmail.com>
2021-03-01 10:58:01 -0600

    Translated Fizzbuzz JS to C#

--- fizzbuzz/csharp/Fizbuzz/Program.cs
+++ fizzbuzz/csharp/Fizbuzz/Program.cs
@@ -4,9 +4,27 @@ namespace Fizbuzz
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            for (int n = 1; n <= 100; n++)
+            {
+                if (n % 15 == 0)
+                {
+                    Console.WriteLine("fizzbuzz");
+                }
+                else if (n % 3 == 0)
+                {
+                    Console.WriteLine("fizz");
+                }
+                else if (n % 5 == 0)
+                {
+                    Console.WriteLine("buzz");
+                }
+                else
+                {
+                    Console.WriteLine(n);
+                }
+            }
         }
     }
 }
